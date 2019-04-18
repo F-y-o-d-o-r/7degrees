498cdb39197cf7e9b8766eec76b9aded203bd291
Fyodor <fyodor.work@gmail.com>
2019-04-18 11:45:55 +0300

    react-intl

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
 
 class App extends Component {
   render() {
@@ -8,15 +9,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <FormattedMessage
+            id="app.title"
+            defaultMessage="Welcome to {what}"
+            description="привет 1"
+            values={{ what: 'react-intl' }}
+          />
+          <FormattedHTMLMessage
+            id="app.intro"
+            defaultMessage="111111To get started, edit <code>src/App.js</code> and save to reload."
+            description="Text on main page"
+          />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { IntlProvider, addLocaleData } from 'react-intl';
+import locale_en from 'react-intl/locale-data/en';
+import locale_ru from 'react-intl/locale-data/ru';
+import messages_en from './translations/en.json';
+import messages_ru from './translations/ru.json';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+addLocaleData([ ...locale_en, ...locale_ru ]);
+const messages = {
+  ru: messages_ru,
+  en: messages_en
+};
+const language = navigator.language.split(/[-_]/)[0];
+
+ReactDOM.render(
+  <IntlProvider locale={language} messages={messages[language]}>
+    <App />
+  </IntlProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
