0acb31976852f5a6970cb3963d5863a6a04df042
Fyodor <fyodor.work@gmail.com>
2019-04-22 17:58:07 +0300

    about start

--- src/Screens/about/AboutScreen.js
+++ src/Screens/about/AboutScreen.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Header } from './components';
 
 class AboutScreen extends Component {
   render() {
     return (
-      <div>
-        <h1>about</h1>
-      </div>
+      <section className="screen-about">
+        <Header />
+      </section>
     );
   }
 }
