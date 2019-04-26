816ee390f7f3dc5d4dd0c1ed20f49833d995ab1f
Fyodor <fyodor.work@gmail.com>
2019-04-26 15:08:45 +0300

    slider done

--- src/Screens/index/components/Slider.js
+++ src/Screens/index/components/Slider.js
@@ -35,8 +35,35 @@ class SliderMain extends Component {
     }
     // END for ie
   }
+  _onDotsClick = (e) => {
+    // this._imageScale();
+    this.slider.slickPause();
+    setTimeout(() => {
+      this.slider.slickPlay();
+    }, 2000);
+    let buttons = document.querySelectorAll('.main-slider-wrapper .slick-dots li button');
+    for (let i = 0; i < buttons.length; i++) {
+      setTimeout(() => {
+        buttons[i].classList.remove('slick-was-before');
+        if (i < +e.target.innerText) {
+          buttons[i].classList.add('slick-was-before');
+        }
+      }, 10);
+    }
+  };
+
+  _imageScale = () => {
+    setTimeout(() => {
+      let allSlides = document.querySelectorAll('.main-slider-wrapper img');
+      let activeImg = document.querySelector('.main-slider-wrapper .slick-active img');
+      for (let i = 0; i < allSlides.length; i++) {
+        allSlides[i].style.transform = 'scale(1)';
+      }
+      activeImg.style.transform = 'scale(1.3)';
+    }, 2000);
+  };
+
   render() {
-    // var buttons = document.querySelectorAll('.main-slider-wrapper .slick-dots li button');
     // https://react-slick.neostack.com/docs/api
     var settings = {
       autoplay: true,
@@ -50,17 +77,23 @@ class SliderMain extends Component {
       adaptiveHeight: false,
       arrows: false,
       pauseOnHover: false,
-      onInit: () =>
+      onInit: () => {
         setTimeout(() => {
           let buttons = document.querySelectorAll('.main-slider-wrapper .slick-dots li button');
+          for (let i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener('click', this._onDotsClick);
+          }
+
           buttons[0].classList.add('slick-was-before');
           let firstImg = document.querySelector('.main-slider-wrapper .slick-active img');
           if (firstImg) {
             firstImg.style.transform = 'scale(1.3)';
           }
-        }, 2000),
+        }, 2000);
+      },
       beforeChange: (current, next) => {
         let buttons = document.querySelectorAll('.main-slider-wrapper .slick-dots li button');
+        buttons[current].classList.add('slick-was-before');
         if (current === buttons.length - 1) {
           for (let i = 0; i < buttons.length; i++) {
             buttons[i].classList.remove('slick-was-before');
@@ -68,18 +101,11 @@ class SliderMain extends Component {
         }
       },
       afterChange: (current) => {
-        setTimeout(() => {
-          let buttons = document.querySelectorAll('.main-slider-wrapper .slick-dots li button');
-          buttons[current].classList.add('slick-was-before');
-
-          let allSlides = document.querySelectorAll('.main-slider-wrapper img');
-          let activeImg = document.querySelector('.main-slider-wrapper .slick-active img');
-          for (let i = 0; i < allSlides.length; i++) {
-            allSlides[i].style.transform = 'scale(1)';
-          }
-          activeImg.style.transform = 'scale(1.3)';
-        }, 2000);
-        // END img scale
+        let allSlides = document.querySelectorAll('.main-slider-wrapper img');
+        for (let i = 0; i < allSlides.length; i++) {
+          allSlides[i].style.transform = 'scale(1)';
+        }
+        this._imageScale();
       }
     };
     return (
@@ -87,7 +113,7 @@ class SliderMain extends Component {
         <div className="container">
           <h1>Мы – Seven Degrees. Создаем digital-продукты для бизнеса</h1>
         </div>
-        <Slider {...settings}>
+        <Slider ref={(slider) => (this.slider = slider)} {...settings}>
           <div className="slider-item-wrapper">
             <img src={one} alt="slider" />
             <div className="bg" />
